5e9d699a85faedeaa316cd12c3aeff195384b9e5
itsiospaschalis <49912151+itsiospaschalis@users.noreply.github.com>
2020-07-01 00:01:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -13,9 +13,9 @@ sess = tf.Session()
 # ή αυτο (1)
 session=tf.compat.v1.Session()
 saver = tf.train.import_meta_graph("C:\\Users\\itsios\\Desktop\\dissertation\\checkpoints\\model.ckpt-1495066.meta")
-saver.restore(sess,tf.train.latest_checkpoint("C:\\Users\\itsios\\Desktop\\dissertation\\checkpoints\\model.ckpt-1495066.txt\\.\\"))
+saver.restore(sess,tf.train.latest_checkpoint("C:\\Users\\itsios\\Desktop\\dissertation\\checkpoints\\model.ckpt-1495066.txt"))
 
 #ή αυτο  (2)
 with tf.Session() as sess:
   new_saver = tf.train.import_meta_graph("C:\\Users\\itsios\\Desktop\\dissertation\\checkpoints\\model.ckpt-1495066.meta")
-  new_saver.restore(sess, tf.train.latest_checkpoint('C:/Users/itsios/Desktop/dissertation/checkpoints/model.ckpt-1495066.txt./'))
+  new_saver.restore(sess, tf.train.latest_checkpoint('C:\\Users\\itsios\\Desktop\\dissertation\\checkpoints\\model.ckpt-1495066.txt'))
