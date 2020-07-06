9ffb1e509ac139598a70b9b300f71afcde756215
itsiospaschalis <49912151+itsiospaschalis@users.noreply.github.com>
2020-07-06 04:27:08 +0300

    Add files via upload

---
+++ image_preprocessing_pca.py
@@ -0,0 +1,58 @@
+import cv2
+import glob
+images = [cv2.imread(file) for file in glob.glob("C:\\Users\\itsios\\Desktop\\dissertation\\COVID\\*.png")]
+
+images[45].shape
+colour=images[45]
+gray = cv2.cvtColor(images[45],cv2.COLOR_BGR2GRAY)
+img4= images[45].reshape((-1,1))
+img5=images[45].reshape((-1,3))
+
+
+cv2.imshow('image',d)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+import numpy as np
+
+X=[]
+
+for i in range(1,1252):
+    gray=cv2.resize(images[i],(224,224))
+    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
+    gray=gray.reshape((-1,1))
+    X.append(gray)
+      
+
+y=[]
+
+for i in range(1,1252):
+    gray=cv2.resize(images[i],(224,224))
+    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
+    gray = gray.flatten()
+    y.append(gray)
+
+
+#transform it to a pandas dataframe
+
+import pandas as pd
+df = pd.DataFrame(y)  
+
+#PCA 
+
+from sklearn . decomposition import PCA
+
+pca = PCA(n_components =5)
+pca.fit ( df )
+Coeff = pca. components_
+print(Coeff)
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.plot(list(pca.explained_variance_ratio_),'-o')
+plt.title('Explained variance ratio as function of PCA components')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Component')
+plt.savefig("Figure2.png")
+plt.show()
